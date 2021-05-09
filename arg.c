a63b1992355b9320016342016d5e61a519f23123
Daniel Raj D'Luccas <daniel.dluccas@gmail.com>
2021-05-09 14:57:50 -0300

    Add files via upload
    tentativa de matriz dinamica

--- arg.c
+++ arg.c
@@ -3,12 +3,17 @@
 #include <string.h>
 
 
+void controlematriz(int);
+//void setmatriz (char *, char[][10]);
 
 int main (int argc, char * argv[])
 {	
 	int padrao = 1;
 	char r;
 	char s[3];
+	//char basem[10][10];
+	//char** p;
+	
 	
 	while(--argc>0){													//apuração dos parametros
 			r = (*++argv)[0];
@@ -24,6 +29,10 @@ int main (int argc, char * argv[])
 					printf("configuracao 2\n");							//INPUT DA CONFIGURAÇÃO 2
 					padrao = 0;
 					}
+				else if ((strcmp(s,"c2")!=0) && (strcmp(s,"c2")!=0)){
+					printf("configuração invalida\n");
+					padrao = -1;
+				}
 				else if(strcmp(s,"m")==0){								//INPUT DE MOVIMENTAÇÃO
 					int x,y,d;											//COORDENADAS x e Y. DIRECAO D
 
@@ -39,26 +48,48 @@ int main (int argc, char * argv[])
 	}
 	
 	controlematriz(padrao);
+	
 }
 
 
 void controlematriz (int p){
 
-if (p == 1){
-printf("configuracao 1\n");
+char matriz1[][7] =  {"******\n",
+					  "*aDDb*\n",
+					  "*aDDb*\n",
+					  "*cdde*\n",
+					  "*cghe*\n",
+					  "*f00i*\n",
+					  "**  **\n"};
+
+char matriz2[][9] = {"********\n",
+					  "*DD0aab*\n",
+					  "*DD0acd*\n",
+					  "*eefgdd*\n",
+					  "*hhijkl \n",
+					  "*hiimkl \n",
+					  "********\n"};
+
+
+	if (p == 1){
+		printf("configuracao 1\n");
+		//setmatriz(matriz1,m);
+	}
+
+	
+
+	else if (p == 0){
+		printf("configuracao 2\n");
+		//setmatriz(matriz2,m);
+	}
+						 
+	else if (p==-1){
+	printf("configuração invalida\n");
+	}
 
 }
 
-else if (p == 0){
-printf("configuracao 2\n");
-}
+/*void setmatriz(char * a, char *b){
+	
 
-char matriz1[][9] = {"********",
-					"*DD aab*",
-					"*DD acd*",
-					"*eefgdd*",
-					"*hhijkl ",
-					"*hiimkl ",
-					"********",};
-
-}
+}*/
